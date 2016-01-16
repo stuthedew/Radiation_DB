19e573f26d6bdd23dbcb98d23814a66598242979
Stuart Feichtinger <stuart.feichtinger@gmail.com>
2016-01-16 14:57:08 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+'''MQTT and MySQL based radiation sensor logger'''
+import sys
+
+
+
+def main():
+    """Wait for incoming radiation data, and log it to MySQL3 database"""
+    #connect to MySQL database
+    #start mqtt server
+    #wait for data
+    #if data then log to MySQL database
+
+    pass
+
+if __name__ == '__main__':
+    sys.exit(main())
