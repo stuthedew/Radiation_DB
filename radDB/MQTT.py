d2aff5d34af3854130aa58f7b2bc774b0a5cf07a
Stuart Feichtinger <stuart.feichtinger@gmail.com>
2016-01-17 13:35:51 -0600

    Tweaks

--- radDB/MQTT.py
+++ radDB/MQTT.py
@@ -1,25 +1,55 @@
 """MQTT function helper"""
 import paho.mqtt.client as mqtt
+import sys
+
+feed_id= "test"
+
+return_str =[
+    "Connection successful",
+    "incorrect protocol version",
+    "invalid client identifier",
+    "server unavailable",
+    "bad username or password",
+    "not authorised"
+    ]
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, rc):
-    print("Connected with result code "+str(rc))
+    """callback function on connect. Subscribes or exits depending on outcome"""
+    print(return_str[rc])
+    if(rc > 1):
+        print("Connection refused - " + return_str[rc])
+        sys.exit(rc)
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
-    client.subscribe("test/#")
+    else:
+        print(return_str[rc])
+        client.subscribe(feed_id)
+
+def on_disconnect(client, userdata, rc):
+    """Callback for disconnect"""
+    if rc != 0:
+        print("Unexpected disconnection.")
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload))
 
-client = mqtt.Client()
-client.on_connect = on_connect
-client.on_message = on_message
-client.username_pw_set('radpi', 'testpassword')
-client.connect("localhost", 1883, 60)
+def main():
+    """MQTT test function"""
+    client = mqtt.Client()
+    client.on_connect = on_connect
+    client.on_message = on_message
+    client.username_pw_set('radpi', 'testpassword')
+    client.connect("localhost", 1883, 60)
 
-# Blocking call that processes network traffic, dispatches callbacks and
-# handles reconnecting.
-# Other loop*() functions are available that give a threaded interface and a
-# manual interface.
-client.loop_forever()
+    # Blocking call that processes network traffic, dispatches callbacks and
+    # handles reconnecting.
+    # Other loop*() functions are available that give a threaded interface and a
+    # manual interface.
+    client.loop_forever()
+
+
+
+if __name__ == '__main__':
+    sys.exit(main())
