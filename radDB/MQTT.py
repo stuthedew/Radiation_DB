0727df4d17d1baeae40fea288c3e3be63821c737
Stuart Feichtinger <stuart.feichtinger@gmail.com>
2016-01-16 22:16:59 -0600

    Added username

--- radDB/MQTT.py
+++ radDB/MQTT.py
@@ -15,7 +15,7 @@ def on_message(client, userdata, msg):
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
-
+mqtt.username_pw_set(radpi, password="testpassword")
 client.connect("localhost", 1883, 60)
 
 # Blocking call that processes network traffic, dispatches callbacks and
